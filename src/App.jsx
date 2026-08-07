e0dbf37106f909cb17e825678bc0eeb0d13a5737
aimanaziz <aimanaziz2000066@gmail.com>
2026-08-07 19:39:50 +0500

    Add First Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+//function App(){
+//return(
+//<h1>Hello React.js!</h1>
+  //)
+//}
+//export default App;
+
 function App(){
-return(
-<h1>Hello React.js!</h1>
+  return(
+    <h1>First Component</h1>
   )
 }
 export default App;
