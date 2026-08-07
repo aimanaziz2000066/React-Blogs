ea8f29bd57eb9b6f517a15f3f714c64763ef85f9
aimanaziz <aimanaziz2000066@gmail.com>
2026-08-07 19:45:04 +0500

    Update JSX component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,25 @@
 //}
 //export default App;
 
-function App(){
-  return(
-    <h1>First Component</h1>
-  )
+//function App(){
+  //return(
+    //<h1>First Component</h1>
+  //)
+//}
+//export default App;
+
+
+//2: JSX with Variables
+function App() {
+  const name = "Aiman";
+  const age = 20;
+
+  return (
+    <>
+      <h1>My Name is {name}</h1>
+      <h2>Age: {age}</h2>
+    </>
+  );
 }
+
 export default App;
